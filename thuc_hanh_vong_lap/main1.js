042676f8c1876cbf8c99649ab02b55f1d9ed7794
sonph <sonph>
2022-05-11 15:57:28 +0700

    vong lap

---
+++ thuc_hanh_vong_lap/main1.js
@@ -0,0 +1,9 @@
+let text = "";
+let cars = ["c","o", "d", "e", "g", "y", "m"];
+text += cars[0] + "<br>";
+text += cars[1] + "<br>";
+text += cars[2] + "<br>";
+text += cars[3] + "<br>";
+text += cars[4] + "<br>";
+text += cars[5] + "<br>";
+document.write(text);
